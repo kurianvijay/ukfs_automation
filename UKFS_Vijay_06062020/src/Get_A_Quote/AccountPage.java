ebf0b57ed8d5889c84df1552d3a283d13969c632
Vijay Kurian <kurianvijay7@gmail.com>
2020-06-17 21:55:13 +0100

    Added methods to checkout to AccountPage class

--- UKFS_Vijay_06062020/src/Get_A_Quote/AccountPage.java
+++ UKFS_Vijay_06062020/src/Get_A_Quote/AccountPage.java
@@ -5,6 +5,8 @@ package Get_A_Quote;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 /**
  * @author vijaykurian
@@ -14,12 +16,47 @@ public class AccountPage {
 	
 	WebDriver driver;
 	
-	public HomePage(WebDriver driver) 
+	Actions actions = new Actions(driver);
+	
+	public AccountPage(WebDriver driver) 
 	{
 		this.driver = driver;
 	}
 	
-	By corkWallTiles = By.linkText("Cork & Wall Tiles");
+	By corkWallTiles = By.xpath("//ul[@id='nav']/li[6]/a/span");
+	
+	By vitaDecor = By.xpath("//ul/li[6]/div/ul/ul/div/li[4]/a/span");
+	
+	By addToCartCliceNatural = By.xpath("//div[2]/div/button/span/span"); //list pick one
+	
+	By summaryRow = By.xpath("//div[3]/div/div/div/div");
+	
+	By mainCheckout = By.xpath("//span/span[1]"); //list pick first one 
+	
+	
+	public void hovercorkwalltilesandClickVitaDecor() {
+		
+		WebElement menuOption = driver.findElement(corkWallTiles);
+		actions.moveToElement(menuOption).perform();
+		
+		WebElement vitaDecorOption = driver.findElement(vitaDecor);
+		vitaDecorOption.click();
+	}
+	
+	public void addToCart() {
+		
+		driver.findElement(addToCartCliceNatural).click();
+	}
+	
+	public void goToCheckoutMain() {
+		
+		driver.findElement(summaryRow).click();
+		driver.findElement(mainCheckout).click();
+		
+	}
+	
+	
+
 	
 	
 }
